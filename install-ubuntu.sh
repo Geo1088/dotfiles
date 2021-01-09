1b8fc7a54fd956ed66176311866d93d20a3e9fbf
Geo <georgej1088@gmail.com>
2021-01-08 19:51:27 -0500

    yeet thefuck, less opinions about git/node

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -6,12 +6,7 @@ sudo apt upgrade
 sudo apt install git curl
 
 # Install nvm and use it to install node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 14
-node --version
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 # Install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -20,15 +15,9 @@ sudo apt update
 sudo apt install --no-install-recommends yarn
 yarn --version
 
-# Install thefuck
-sudo apt install python3-dev python3-pip python3-setuptools
-sudo pip3 install thefuck
-
 # Set up git config and aliases
-git config --global user.name "George"
-git config --global user.email "georgej1088@gmail.com"
-git config --global credential.helper store # Cache remote credentials
 git config --global alias.l "log --oneline --decorate --graph"
+git config --global alias.la "log --oneline --decorate --graph --all"
 
 # Install zsh and set it as the default shell
 sudo apt install zsh
